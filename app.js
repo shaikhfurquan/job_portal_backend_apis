7bf0d963f6cb2992a64f8f77fec638e654e2dc32
shaikhfurquan <shaikhfurquan313@gmail.com>
2024-01-25 18:52:22 +0530

    cors morgan

--- app.js
+++ app.js
@@ -1,19 +1,32 @@
 import express from 'express';
 import dotenv from 'dotenv';
-
+import cors from 'cors';
+import morgan from 'morgan';
+import connectDB from './db/connectDB.js';
+import userRouter from './routes/userRouter.js';
+userRouter
 dotenv.config()
+
 
 const app = express();
 const PORT = process.env.PORT || 5000
 
 
-//routes
-app.get('/', (req, res)=>{
-    res.json({
-        messsage : "Welcome"
-    })
-})
+//Express middlewares
+app.use(express.json());
+app.use(cors());
+app.use(morgan('dev'))
 
+
+
+//routes
+app.use('/api/v1/user' , userRouter)
+
+
+//DB Configuration
+connectDB()
+
+//listening port
 app.listen(PORT, ()=>{
     console.log(`Server listening on ${PORT}`);
 })
