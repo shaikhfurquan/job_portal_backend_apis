fc9843f1636a3d5a0ffa85cb8e9ec778bb62cefa
shaikhfurquan <79248431+shaikhfurquan@users.noreply.github.com>
2024-01-25 19:58:38 +0530

    Delete routes/userRouter.js

--- routes/userRouter.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import { getUser } from '../controllers/userController.js'
-const userRouter = express.Router()
-
-
-userRouter.get('/' , getUser)
-
-
-export default userRouter
