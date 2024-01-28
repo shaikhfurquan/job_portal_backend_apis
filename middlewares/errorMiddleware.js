10565c075019f0e166ad1cfe3d310d76ff3f3972
shaikhfurquan <shaikhfurquan313@gmail.com>
2024-01-28 12:08:34 +0530

    status update

--- middlewares/errorMiddleware.js
+++ middlewares/errorMiddleware.js
@@ -3,7 +3,7 @@
 
 const errorMiddleware = (err, req, res, next) => {
     console.log(err);
-    return res.status(500).json({
+    return res.status(400).json({
         success: false,
         message: "Something went wrong",
         err
