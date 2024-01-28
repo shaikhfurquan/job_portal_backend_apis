69930c9d53bbac1bc3fe8220f752f8e7e7208fdc
shaikhfurquan <shaikhfurquan313@gmail.com>
2024-01-28 13:57:42 +0530

    authenticate user

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,1 +1,26 @@
 import JWT from 'jsonwebtoken'
+
+const userAuth = async (req, res, next) => {
+    try {
+        const authHeaders = req.headers.authorization
+        if (!authHeaders || !authHeaders.startsWith('Bearer')) {
+            next('Authentication failed Failed, There is no token available')
+        }
+
+        const token = authHeaders.split(' ')[1]
+        
+        const payload = JWT.verify(token, process.env.JWT_SECRET)
+        req.user = { userId: payload.userId }
+        next()
+
+    } catch (error) {
+        return res.status(400).json({
+            success: false,
+            message: "Authentication failed",
+            error: error.message
+        })
+    }
+}
+
+
+export default userAuth
