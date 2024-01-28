81db240625a05f40adeda93a252337668bdbefcc
shaikhfurquan <shaikhfurquan313@gmail.com>
2024-01-28 12:08:15 +0530

    changes

--- db/connectDB.js
+++ db/connectDB.js
@@ -3,9 +3,9 @@ import mongoose from "mongoose";
 const connectDB = async () =>{
     try {
         await mongoose.connect(process.env.MONGO_URL)
-        console.log(`connected to db ==> ${process.env.MONGO_URL} successfully`);
+        console.log(`connected to db successfully ==> ${process.env.MONGO_URL} `);
     } catch (error) {
-        console.log('Error connecting to DB ==>' , error.message);
+        console.log('Error connecting to DB ==> ' , error.message);
     }  
 }
 
