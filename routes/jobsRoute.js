56214a74c51c301fdc35b3ad2803e5191971c575
shaikhfurquan <shaikhfurquan313@gmail.com>
2024-01-31 12:16:55 +0530

    update and delete

--- routes/jobsRoute.js
+++ routes/jobsRoute.js
@@ -1,9 +1,12 @@
 import express  from "express";
 import isAuthenticated from "../middlewares/authMiddleware.js";
-import { createJob } from "../controllers/jobController.js";
+import { createJob, getAllJob, updateJobById , deleteJobById} from "../controllers/jobController.js";
 
 const jobRouter = express.Router()
 
 jobRouter.post('/create-job' , isAuthenticated, createJob)
+jobRouter.get('/get-job' , isAuthenticated, getAllJob)
+jobRouter.patch('/update-job/:id' , isAuthenticated, updateJobById)
+jobRouter.delete('/delete-job/:id' , isAuthenticated, deleteJobById)
 
 export default jobRouter
