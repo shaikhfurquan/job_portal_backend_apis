bf4b4300489ecb92e5d97df89d220f457ac1851c
shaikhfurquan <shaikhfurquan313@gmail.com>
2024-02-01 01:26:08 +0530

    job stat filter

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,4 +1,6 @@
+import mongoose from "mongoose"
 import JobModel from "../models/jobModel.js"
+import moment from "moment"
 
 
 export const createJob = async (req, res, next) => {
@@ -85,8 +87,69 @@ export const deleteJobById = async (req, res, next) => {
         }
         await job.deleteOne()
         res.status(200).json({
-            success : true , 
-            message : 'Job deleted successfully'})
+            success: true,
+            message: 'Job deleted successfully'
+        })
+    } catch (error) {
+        next(error.message)
+    }
+}
+
+
+// jobs state and filters
+export const jobStats = async (req, res, next) => {
+    try{
+    const stats = await JobModel.aggregate([
+        // search by user jobs
+        {
+          $match: {
+            createdBy: new mongoose.Types.ObjectId(req.user.userId),
+          },
+        },
+        {
+          $group: {
+            _id: "$status",
+            count: { $sum: 1 },
+          },
+        },
+      ]);
+    
+      //default stats
+      const defaultStats = {
+        pending: stats.pending || 0,
+        reject: stats.reject || 0,
+        interview: stats.interview || 0,
+      };
+    
+      //monthly yearly stats
+      let monthlyApplication = await JobModel.aggregate([
+        {
+          $match: {
+            createdBy: new mongoose.Types.ObjectId(req.user.userId),
+          },
+        },
+        {
+          $group: {
+            _id: {
+              year: { $year: "$createdAt" },
+              month: { $month: "$createdAt" },
+            },
+            count: {
+              $sum: 1,
+            },
+          },
+        },
+      ]);
+      monthlyApplication = monthlyApplication.map((item) => {
+          const { _id: { year, month }, count, } = item;
+          const date = moment().month(month - 1).year(year).format("MMM Y");
+          return { date, count };
+        }).reverse();
+      res.status(200).json({success : true,
+         totlaJob: stats.length, 
+         defaultStats, 
+         monthlyApplication });
+    
     } catch (error) {
         next(error.message)
     }
