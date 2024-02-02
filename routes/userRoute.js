ab6a024f853748fd4ad190cd23f223c23f327c22
shaikhfurquan <shaikhfurquan313@gmail.com>
2024-02-02 23:59:48 +0530

    rate limit

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,12 +1,22 @@
 import express from 'express'
 import { getUser, loginUser, registerUser, updateUser } from '../controllers/userController.js'
 import isAuthenticated from "../middlewares/authMiddleware.js";
+import { rateLimit } from 'express-rate-limit'
+
 const userRouter = express.Router()
 
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+	// store: ... , // Use an external store for consistency across multiple server instances.
+})
 
-userRouter.post('/register' , registerUser)
+
+userRouter.post('/register' , limiter ,  registerUser)
 userRouter.get('/' , getUser)
-userRouter.post('/login' , loginUser)
+userRouter.post('/login' , limiter ,loginUser)
 userRouter.put('/update' , isAuthenticated ,updateUser)
 
 
