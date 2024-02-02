356f046f0d1c1ec5f34922357537dd69382c1d05
shaikhfurquan <shaikhfurquan313@gmail.com>
2024-02-03 00:01:32 +0530

    helmet pkg

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import morgan from 'morgan';
+// security packages
+import helmet from 'helmet';
+import xss from 'xss-clean'
+import mongoSanitize from 'express-mongo-sanitize';
+
 import connectDB from './db/connectDB.js';
 import userRouter from './routes/userRoute.js';
 import errorMiddleware from './middlewares/errorMiddleware.js';
@@ -15,6 +20,9 @@ const PORT = process.env.PORT || 5000
 
 
 //Express middlewares
+app.use(helmet());      //for header security
+app.use(xss())          //cross side scripting attack protection
+app.use(mongoSanitize()); //for secure express/mongodb 
 app.use(express.json());
 app.use(cors());
 app.use(morgan('dev'))
